3863c93f42acf651352369ff07fcdc14a1077c87
VighneshAnandSherugar <vighnesh.sherugar@webitoinfotech.com>
2026-06-10 14:59:54 +0530

    API|| Added new changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# voice-clone-api
+---
+title: voice-clone-api
+emoji: 🎤
+colorFrom: blue
+colorTo: purple
+sdk: docker
+pinned: false
+---
+
+# Voice Clone API
+
+FastAPI + Coqui XTTS Voice Cloning API
--- app/voice_clone.py
+++ app/voice_clone.py
@@ -1,10 +1,19 @@
+import os
+
+os.environ["COQUI_TOS_AGREED"] = "1"
+
+
+
 from TTS.api import TTS
 import uuid
+
 
 print("Loading XTTS Model...")
 
 tts = TTS(
-    "tts_models/multilingual/multi-dataset/xtts_v2"
+    "tts_models/multilingual/multi-dataset/xtts_v2",
+    progress_bar=False,
+    gpu=False
 )
 
 print("Model Loaded")
@@ -13,8 +22,7 @@ print("Model Loaded")
 def clone_voice(text, speaker_wav):
     
     output_file = f"app/outputs/{uuid.uuid4()}.wav"
-    breakpoint()
-
+    
     tts.tts_to_file(
         text=text,
         speaker_wav=speaker_wav,
